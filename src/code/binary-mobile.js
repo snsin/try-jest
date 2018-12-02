ae3032375d5b8a74d622958145c798604c23c16e
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-02 20:14:00 +0300

    done file exex task

--- src/code/binary-mobile.js
+++ src/code/binary-mobile.js
@@ -1,6 +1,6 @@
-import { cons, car, cdr, toString } from 'hexlet-pairs';
+import {
+  cons, car, cdr, toString 
+} from 'hexlet-pairs';
 
 const mob = cons({ name: 'left' }, 'right');
 console.log(car(mob));
-
-// TODO .vscode/tasks.js to simply launch current file; @esti
