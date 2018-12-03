3b20619a37da64b23263075b66892177de1dff2a
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-03 21:42:01 +0300

    add hexlet-pair-data library

--- src/code/binary-mobile.js
+++ src/code/binary-mobile.js
@@ -2,5 +2,9 @@ import {
   cons, car, cdr, toString 
 } from 'hexlet-pairs';
 
+import { l as list, reduce } from 'hexlet-pairs-data';
+
 const mob = cons({ name: 'left' }, 'right');
 console.log(car(mob));
+console.log(list(1, 2, 3, 4));
+console.log(reduce((i, acc) => [...acc, i], [], list('o', 'my', 'god')));
