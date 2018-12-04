caedfd5873d8e935631605d116a78a651722f2b0
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-04 22:54:29 +0300

    solve ex. 2.29 part a, make test, make selectors

--- __tests__/binary-mobile-test.js
+++ __tests__/binary-mobile-test.js
@@ -1,5 +1,21 @@
 import { l as list } from 'hexlet-pairs-data';
-import { printList } from '../src/code/binary-mobile';
+import {
+  printList,
+  makeBranch,
+  makeMobile,
+  leftBranch,
+  rightBranch,
+  branchLength,
+} from '../src/code/binary-mobile';
+
+const leftBr = makeBranch(10, 6);
+const rightBr = makeBranch(6, 10);
+const leftBr1 = makeBranch(2, 2);
+const rightBr1 = makeBranch(3, 3);
+const complexBr = makeBranch(20, makeMobile(leftBr1, rightBr1));
+const complexBr2 = makeBranch(5, makeMobile(rightBr, rightBr1));
+const mobile = makeMobile(leftBr, rightBr);
+const mobile1 = makeMobile(complexBr, complexBr2);
 
 test('printList', () => {
   expect(printList(list())).toBe('()');
@@ -7,4 +23,20 @@ test('printList', () => {
 
 test('printList2', () => {
   expect(printList(list(1, 2, 3))).toBe('(1, 2, 3)');
+});
+
+describe('2.29 a. left-branch, right-branch, length, and strucrure selectors', () => {
+  test('left branch', () => {
+    expect(leftBranch(mobile)).toBe(leftBr);
+    expect(leftBranch(mobile1)).toBe(complexBr);
+  });
+  test('rigth branch', () => {
+    expect(rightBranch(mobile)).toBe(rightBr);
+    expect(rightBranch(mobile1)).toBe(complexBr2);
+  });
+
+  test('branch length', () => {
+    expect(branchLength(leftBr)).toBe(10);
+    expect(branchLength(rightBr)).toBe(6);
+  });
 });
--- src/code/binary-mobile.js
+++ src/code/binary-mobile.js
@@ -1,12 +1,22 @@
-import { l as list, reduce } from 'hexlet-pairs-data';
+import { l as list, reduce, head, tail, isList } from 'hexlet-pairs-data';
 
-const makeMobile = (left, right) => list(left, right);
-const makeBranch = (length, structure) => list(length, structure);
+export const makeMobile = (left, right) => list(left, right);
+export const makeBranch = (length, structure) => list(length, structure);
 export const printList = (elements) => {
   const arr = reduce((i, acc) => [...acc, i], [], elements);
   return `(${arr.join(', ')})`;
 };
 
+export const leftBranch = (mobile) => {
+  const left = head(mobile);
+  return isList(left) ? left : null;
+};
+export const rightBranch = (mobile) => {
+  const rest = tail(mobile);
+  return isList(rest) ? head(rest) : null;
+};
+export const branchLength = branch => head(branch);
+export const branchStructure = branch => head(tail(branch));
 export const noop = () => {};
 
 console.log(list(1, 2, 3, 4));
