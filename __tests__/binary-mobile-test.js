1879168aa317a7ce6ceb4920d806b84fbc6aba21
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-07 00:15:05 +0300

    add total weight calculation and isBalanced predicate

--- __tests__/binary-mobile-test.js
+++ __tests__/binary-mobile-test.js
@@ -6,16 +6,23 @@ import {
   leftBranch,
   rightBranch,
   branchLength,
+  totalWeight,
+  isBalanced,
 } from '../src/code/binary-mobile';
 
 const leftBr = makeBranch(10, 6);
 const rightBr = makeBranch(6, 10);
 const leftBr1 = makeBranch(2, 2);
 const rightBr1 = makeBranch(3, 3);
+const leftBr2 = makeBranch(4, 3);
+const rightBr2 = makeBranch(2, 6);
 const complexBr = makeBranch(20, makeMobile(leftBr1, rightBr1));
 const complexBr2 = makeBranch(5, makeMobile(rightBr, rightBr1));
+const complexBr3 = makeBranch(4, makeMobile(leftBr2, rightBr2)); // w = 9
+const complexBr4 = makeBranch(5, makeMobile(makeBranch(12, 3), complexBr3)); // w = 12
 const mobile = makeMobile(leftBr, rightBr);
 const mobile1 = makeMobile(complexBr, complexBr2);
+const mobile2 = makeMobile(leftBr, complexBr4);
 
 test('printList', () => {
   expect(printList(list())).toBe('()');
@@ -38,5 +45,20 @@ describe('2.29 a. left-branch, right-branch, length, and strucrure selectors', (
   test('branch length', () => {
     expect(branchLength(leftBr)).toBe(10);
     expect(branchLength(rightBr)).toBe(6);
+  });
+});
+
+describe('2.29 b. total-weight', () => {
+  test('weigth', () => {
+    expect(totalWeight(mobile1)).toBe(18);
+    expect(totalWeight(mobile)).toBe(16);
+  });
+});
+
+describe('2.29 c. is balanced?', () => {
+  test('balanced ?', () => {
+    expect(isBalanced(mobile)).toBeTruthy();
+    expect(isBalanced(mobile1)).toBeFalsy();
+    expect(isBalanced(mobile2)).toBeTruthy();
   });
 });
--- src/code/binary-mobile.js
+++ src/code/binary-mobile.js
@@ -1,4 +1,6 @@
-import { l as list, reduce, head, tail, isList } from 'hexlet-pairs-data';
+import {
+  l as list, reduce, head, tail, isList,
+} from 'hexlet-pairs-data';
 
 export const makeMobile = (left, right) => list(left, right);
 export const makeBranch = (length, structure) => list(length, structure);
@@ -6,6 +8,7 @@ export const printList = (elements) => {
   const arr = reduce((i, acc) => [...acc, i], [], elements);
   return `(${arr.join(', ')})`;
 };
+export const noop = () => {};
 
 export const leftBranch = (mobile) => {
   const left = head(mobile);
@@ -17,7 +20,36 @@ export const rightBranch = (mobile) => {
 };
 export const branchLength = branch => head(branch);
 export const branchStructure = branch => head(tail(branch));
-export const noop = () => {};
+
+export const totalWeight = (mobile) => {
+  const getWeight = elem => (isList(elem) ? totalWeight(elem) : elem);
+  const rightBrElem = branchStructure(rightBranch(mobile));
+  const leftBrElem = branchStructure(leftBranch(mobile));
+  return getWeight(rightBrElem) + getWeight(leftBrElem);
+};
+
+export const isBalanced = (mobile) => {
+  const moment = (branch) => {
+    const brChild = branchStructure(branch);
+    const childWeight = isList(brChild) ? totalWeight(brChild) : brChild;
+    return branchLength(branch) * childWeight;
+  };
+  const leftBr = leftBranch(mobile);
+  const rightBr = rightBranch(mobile);
+  if (moment(leftBr) !== moment(rightBr)) {
+    return false;
+  }
+  const leftChild = branchStructure(leftBr);
+  const rightChild = branchStructure(rightBr);
+  if (isList(leftChild)) {
+    return isBalanced(leftChild);
+  }
+  if (isList(rightChild)) {
+    return isBalanced(rightChild);
+  }
+  return true;
+};
+
 
 console.log(list(1, 2, 3, 4));
 console.log(reduce((i, acc) => [...acc, i], [], makeMobile(makeBranch(10, 6), makeBranch(6, 10))));
